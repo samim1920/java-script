22103e12bca05364c433bdec7ebbf85bbbb05abf
samim1920 <sksamimali2021@gmail.com>
2025-11-13 14:18:19 +0000

    conversion of data type

---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,7 @@
+"use strict" //trewat all js code as newer version
+
+// alert(3+3)
+
+console.log(3+3)
+
+console.log("samim")
---
+++ 01_basic/03_conversionOriantation.js
@@ -0,0 +1,20 @@
+// let score=33;
+let score="33abc";
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let number=Number(score);
+console.log(typeof(number));
+
+console.log(number);
+
+//33->33
+//33abc->Nan
+//true=1; false=0;
+
+let isLoggedIn=1;
+
+let boolIsLoggedIn=Boolean(isLoggedIn);
+console.log(boolIsLoggedIn);
+
